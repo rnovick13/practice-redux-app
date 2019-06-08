24f9d42f11d01196868ea35ba365acceaf8e4cd2
rnovick13 <rnovick13@gmail.com>
2019-06-08 14:29:49 -0400

    worked on PostForm and index

--- src/PostForm.js
+++ src/PostForm.js
@@ -2,13 +2,23 @@ import React, {Component} from 'react'
 
 
 class PostForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const title = this.getTitle.value;
+    const message = this.getMessage.value;
+    const data = {
+      id: new Date(),
+      title,
+      message
+    }
+  }
   render() {
     return (
       <div>
         <h1>Create Post</h1>
-        <form>
-          <input required type="text" placeholder="Enter Post Title" /><br /><br />
-          <textarea required rows="5" cols="28" placeholder="Enter Post" /><br /><br />
+        <form onSubmit={this.handleSubmit}>
+          <input required type="text" ref={(input)=>this.getTitle = input} placeholder="Enter Post Title" /><br /><br />
+          <textarea required rows="5" cols="28" ref={(input)=>this.getMessage = input} placeholder="Enter Post" /><br /><br />
           <button>Post</button>
         </form>
       </div>
