c2dfdb7d7d7811dfb41dae51c15e333e241e3f05
rnovick13 <rnovick13@gmail.com>
2019-06-08 14:21:46 -0400

    added reducer to index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { createStore } from 'redux'
+import postReducer from './reducers/postReducer'
 
-const store = createStore();
+const store = createStore(postReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
