70f6cf6408074870bd7cc34c130afb0ba82e327a
rnovick13 <rnovick13@gmail.com>
2019-06-09 14:27:17 -0400

    edit and update completely built

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -6,6 +6,17 @@ const postReducer = (state = [], action) => {
       return state.filter((post) => post.id !== action.id)
     case 'EDIT_POST':
       return state.map((post)=>post.id === action.id ? {...post,editing: !post.editing} : post)
+    case 'UPDATE':
+      return state.map((post) => {
+        if(post.id === action.id) {
+          return {
+            ...post,
+            title: action.data.newTitle,
+            message: action.data.newMessage,
+            editing: !post.editing
+          }
+        } else return post;
+      })
     default:
       return state;
   }
